4945e27b6f41743e7fe51cb7c80a25c4e5171135
Lee Forest <lee8oi@gmail.com>
2013-12-16 20:57:14 -0500

    Removed redundant dir check. Minor simplifications.

--- main.go
+++ main.go
@@ -36,18 +36,17 @@ func extract(file, dest string) {
 
 	// Iterate through the files in the archive,
 	for _, f := range r.File {
-		name := f.Name
-		split := strings.Split(name, ".")
+		split := strings.Split(f.Name, ".")
 		ext := strings.ToLower(split[len(split)-1])
 		if ext == "ttf" || ext == "otf" {
-			fmt.Printf("extracting font:%s ", name)
+			fmt.Printf("extracting font:%s ", f.Name)
 			src, err := f.Open()
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
 			defer src.Close()
-			dst, err := os.Create(dest + "/" + name)
+			dst, err := os.Create(dest + "/" + f.Name)
 			if err != nil {
 				fmt.Println(err)
 				return
@@ -70,9 +69,6 @@ func main() {
 	filepath.Walk(os.Args[1], func(path string, f os.FileInfo, err error) error {
 		name := f.Name()
 		splitup := strings.Split(name, ".")
-		if f.IsDir() {
-			return nil
-		}
 		if splitup[len(splitup)-1] == "zip" {
 			fmt.Printf("Archive: %s\n", name)
 			extract(os.Args[1]+"/"+name, os.Args[2])
